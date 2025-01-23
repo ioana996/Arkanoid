943b7a2236ef6ad480b475ade5624668b8e0179a
ioana996 <stroineaioana@gmail.com>
2025-01-23 22:07:39 +0200

    solve collisions

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let initialBallY = containerPosition.height - 60;
 let ballX, ballY;
 let XDirection;
 let YDirection;
-let bricksPositions;
+// let bricksPositions;
 let bricks;
 
 const initBallCoordinates = () => {
@@ -30,10 +30,10 @@ const initiBallDirection = () => {
 
 const updateBricksPositions = () => {
   let numOfBricksPerRow = 0;
-  let initialCoordinate = 0;
+  let initialCoordinate = 2 * BRICK_WIDTH;
   bricks.forEach((brick) => {
-    if (numOfBricksPerRow % 16 === 0) {
-      initialCoordinate = 0;
+    if (numOfBricksPerRow % 12 === 0) {
+      initialCoordinate = 2 * BRICK_WIDTH;
     }
     brick.style.left = initialCoordinate + "px";
     initialCoordinate += BRICK_WIDTH;
@@ -58,7 +58,7 @@ const initGameStatus = () => {
   initiBallDirection();
 
   bricks = Array.from(document.getElementsByClassName("brick"));
-  bricksPositions = bricks.map((brick) => brick.getBoundingClientRect());
+  // bricksPositions = bricks.map((brick) => brick.getBoundingClientRect());
   updateBricksPositions();
 
   updateBallPosition(ballX, ballY);
@@ -66,86 +66,114 @@ const initGameStatus = () => {
 };
 
 const isInsideCollisionZone = (brick) => {
+  /* considers that each brick has a collision zone which is the area of the brick
+  plus the radius of the ball */
   if (
     ball.offsetLeft >= brick.offsetLeft - BALL_WIDTH &&
     ball.offsetLeft + BALL_WIDTH <=
       brick.offsetLeft + BRICK_WIDTH + BALL_WIDTH &&
     ball.offsetTop >= brick.offsetTop - BALL_WIDTH &&
-    ball.offsetTop <= brick.offsetTop + BRICK_HEIGHT
+    ball.offsetTop + BALL_WIDTH <= brick.offsetTop + BRICK_HEIGHT + BALL_WIDTH
   ) {
-    if (
-      ball.offsetLeft + BALL_WIDTH == brick.offsetLeft ||
-      ball.offsetLeft == brick.offsetLeft + BRICK_WIDTH
-    ) {
-      XDirection = -XDirection;
-    }
-    if (
-      ball.offsetTop + BALL_WIDTH == brick.offsetTop ||
-      ball.offsetTop == brick.offsetTop + BRICK_HEIGHT
-    ) {
-      YDirection = -YDirection;
-    }
-    brick.style.display = "none";
-
-    // brick.style.display = "none";
+    return true;
   }
+  return false;
+};
+
+const isTopCollision = (brick) => {
+  if (
+    ball.offsetTop + BALL_WIDTH >= brick.offsetTop &&
+    ball.offsetLeft + BALL_WIDTH / 2 >= brick.offsetLeft &&
+    ball.offsetLeft + BALL_WIDTH / 2 <= brick.offsetLeft + BRICK_WIDTH &&
+    YDirection > 0
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isBottomColision = (brick) => {
+  if (
+    ball.offsetTop <= brick.offsetTop + BRICK_HEIGHT &&
+    ball.offsetLeft + BALL_WIDTH / 2 >= brick.offsetLeft &&
+    ball.offsetLeft + BALL_WIDTH / 2 <= brick.offsetLeft + BRICK_WIDTH &&
+    YDirection < 0
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isRightCollision = (brick) => {
+  if (
+    ball.offsetLeft <= brick.offsetLeft + BRICK_WIDTH &&
+    ball.offsetTop - BALL_WIDTH / 2 >= brick.offsetTop &&
+    ball.offsetTop + BALL_WIDTH / 2 <= brick.offsetTop + BRICK_HEIGHT &&
+    XDirection < 0
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isLeftCollision = (brick) => {
+  if (
+    ball.offsetLeft + BALL_WIDTH >= brick.offsetLeft &&
+    ball.offsetTop - BALL_WIDTH / 2 >= brick.offsetTop &&
+    ball.offsetTop + BALL_WIDTH / 2 <= brick.offsetTop + BRICK_HEIGHT &&
+    XDirection > 0
+  ) {
+    return true;
+  }
+  return false;
+};
+
+const isCornerCollision = (brick) => {
+  if (
+    // bottom right collision
+    (ball.offsetLeft + BALL_WIDTH / 2 === brick.offsetLeft + BRICK_WIDTH &&
+      ball.offsetTop + BALL_WIDTH / 2 === brick.offsetTop + BRICK_HEIGHT) ||
+    // top right collision
+    (ball.offsetLeft + BALL_WIDTH / 2 === brick.offsetLeft + BRICK_WIDTH &&
+      ball.offsetTop + BALL_WIDTH / 2 === brick.offsetTop) ||
+    // top left collision
+    (ball.offsetLeft + BALL_WIDTH / 2 === brick.offsetLeft &&
+      ball.offsetTop + BALL_WIDTH / 2 === brick.offsetTop) ||
+    // left bottom collision
+    (ball.offsetLeft + BALL_WIDTH / 2 === brick.offsetLeft &&
+      ball.offsetTop + BALL_WIDTH / 2 === brick.offsetTop + BRICK_HEIGHT)
+  ) {
+    return true;
+  }
+  return false;
 };
 
 const checkBallAndBrickCollisions = () => {
-  //   bricks = Array.from(document.getElementsByClassName("brick"));
-
-  //   console.log("Length", bricks.length);
-  //   console.log(bricks[1].offsetLeft);
-  //   console.log(bricks[1].offsetTop);
-  //   console.log(ball.offsetLeft);
-  //   console.log(ball.offsetTop - 20);
   for (let i = 0; i < bricks.length; i++) {
-    // if (
-    //   ball.offsetLeft >= bricks[i].offsetLeft - BALL_WIDTH &&
-    //   ball.offsetLeft + BALL_WIDTH <=
-    //     bricks[i].offsetLeft + BRICK_WIDTH + BALL_WIDTH &&
-    //   ball.offsetTop >= bricks[i].offsetTop - BALL_WIDTH &&
-    //   ball.offsetTop <= bricks[i].offsetTop + BRICK_HEIGHT
-    // ) {
-    //   if (
-    //     ball.offsetLeft + BALL_WIDTH == bricks[i].offsetLeft ||
-    //     ball.offsetLeft == bricks[i].offsetLeft + BRICK_WIDTH
-    //   ) {
-    //     XDirection = -XDirection;
-    //   }
-    //   if (
-    //     ball.offsetTop == bricks[i].offsetTop + BRICK_HEIGHT &&
-    //     i - 1 >= 0 &&
-    //     ball.offsetTop == bricks[i - 1].offsetTop + BRICK_HEIGHT
-    //   ) {
-    //     YDirection = -YDirection;
-    //   }
-    //   if (ball.offsetTop + BALL_WIDTH == bricks[i].offsetTop) {
-    //     YDirection = -YDirection;
-    //   }
-    //   bricks[i].style.display = "none";
-    // }
-    isInsideCollisionZone(bricks[i]);
-    // if (
-    //   bricks[i].offsetTop - 40 === ball.offsetTop &&
-    //   ball.offsetLeft >= bricks[i].offsetLeft &&
-    //   ball.offsetLeft + 20 <= bricks[i].offsetLeft + 80
-    // ) {
-    //   //   bricks[i].remove();
-    //   bricks[i].style.display = "none";
-    // }
+    const brick = bricks[i];
+    if (isInsideCollisionZone(brick)) {
+      if (isTopCollision(brick)) {
+        bricks[i].style.display = "none";
+        YDirection = -YDirection;
+      }
+      if (isBottomColision(brick)) {
+        bricks[i].style.display = "none";
+        YDirection = -YDirection;
+      }
+      if (isRightCollision(brick)) {
+        bricks[i].style.display = "none";
+        XDirection = -XDirection;
+      }
+      if (isLeftCollision(brick)) {
+        bricks[i].style.display = "none";
+        XDirection = -XDirection;
+      }
+      if (isCornerCollision(brick)) {
+        bricks[i].style.display = "none";
+        XDirection = -XDirection;
+      }
+    }
   }
-
-  //   for (let i = 0; i < bricks.length; i++) {
-  //     console.log("i was here ", bricksPositions[i].bottom, ballY);
-  //     if (
-  //       Math.trunc(bricksPositions[i].bottom) === Math.trunc(ballY - 20) &&
-  //       Math.trunc(ballX) >= Math.trunc(bricksPositions[i].left) &&
-  //       Math.trunc(ballX) <= Math.trunc(bricksPositions[i].right)
-  //     ) {
-  //       bricks[i].remove();
-  //     }
-  //   }
 };
 
 function moveBall() {
